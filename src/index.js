de58fb2c7d05e743ea700216b35dcec158da4cfe
Mirele Fernandes <mirele.mrf@gmail.com>
2023-05-10 17:34:36 -0300

    Get current location

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+let headerCities = ["Lisbon", "Paris", "Sydney", "San Francisco", "Fortaleza"];
+
 let currentCityTemperature = document.querySelector(
   "#current_temperature_number"
 );
@@ -30,12 +32,36 @@ curretTime.innerHTML = formatDate(new Date());
 function changeCity(event) {
   event.preventDefault();
 
-  let newCity = document.querySelector("#new_city");
+  let newCity = document.querySelector("#new_city").value;
   let city = document.querySelector("#city_choose");
 
-  city.innerHTML = newCity.value.trim();
+  let cityCorreted = (newCity.trim())[0].toUpperCase() + (newCity.trim()).substring(1);
+  
+  city.innerHTML = cityCorreted;
 }
 
 let inputCity = document.querySelector("#input_city");
 inputCity.addEventListener("submit", changeCity);
+// Get current temperature
+
+
+// Get current location
+function getCurrentPosition(position){
+  let apiKey = "b35c686ba9565ba0ab254c2230937552";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+
+  let currentCity = document.querySelector("#current_city")
+
+
+  axios.get(url).then(showWeather);
+}
+
+navigator.geolocation.getCurrentPosition(getCurrentPosition);
+
+let currentButton = document.querySelector("#button_current");
+currentButton.addEventListener("click", getCurrentPosition);
+
+
 
