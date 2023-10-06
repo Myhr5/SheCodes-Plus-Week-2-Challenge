664db181af8afcd9effe17a39add3b8e4f2e28df
Mirele Fernandes <mirele.mrf@gmail.com>
2023-10-06 14:55:30 -0300

    final_project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 let apiKey = "b35c686ba9565ba0ab254c2230937552";
-let headerCities = ["Lisbon", "Paris", "Sydney", "San Francisco", "Fortaleza"];
 
 let currentCityTemperature = document.querySelector(
   "#current_temperature_number",
@@ -19,6 +18,7 @@ function formatDate(newDate) {
     "Friday",
     "Saturday",
   ];
+
   let day = days[newDate.getDay()];
 
   let hour = newDate.getHours();
@@ -33,17 +33,24 @@ curretTime.innerHTML = formatDate(new Date());
 function currentWeather(response) {
   let currentCityElement = document.querySelector("#current_city");
   let currentIconElement = document.querySelector("#current_icon");
+  let currentTemperature = document.querySelector("#current_temperature");
+  let currentUnit = document.querySelector("#current_unit");
+
   let temperature = Math.round(response.data.main.temp);
 
-  celsiusCurrentTemperature = Math.round(response.data.main.temp);
   currentCity = response.data.name;
 
   let cityElement = document.querySelector("#city_choose");
 
   console.log(response);
 
-  currentCityElement.innerHTML = `${response.data.name} ${temperature} °C`;
+  currentCityElement.innerHTML = response.data.name;
+  currentTemperature.innerHTML = temperature;
   cityElement.innerHTML = response.data.name;
+
+  if (unit === "metric") currentUnit.innerHTML = " °C";
+  else if (unit === "imperial") currentUnit.innerHTML = " °F";
+
   currentIconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
@@ -57,7 +64,7 @@ function currentWeather(response) {
 function getUrlPosition(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
-  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${unit}&appid=${apiKey}`;
 
   axios.get(url).then(currentWeather);
 }
@@ -83,28 +90,37 @@ function changeWeatherInformations(response) {
   let weatherDescription = document.querySelector("#weather_description");
   let humidity = document.querySelector("#humidity");
   let wind = document.querySelector("#wind");
+  let windUnitElement = document.querySelector("#wind_unit");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
-  celsiusTemperature = Math.round(response.data.main.temp);
 
   weatherDescription.innerHTML =
     response.data.weather[0].description[0].toUpperCase() +
     response.data.weather[0].description.substring(1);
   humidity.innerHTML = response.data.main.humidity;
-  wind.innerHTML = Math.round(response.data.wind.speed * 3.6);
+
+  if (unit === "metric") {
+    wind.innerHTML = Math.round(response.data.wind.speed * 3.6);
+    windUnitElement.innerHTML = "km/h";
+  } else if (unit === "imperial") {
+    wind.innerHTML = Math.round(response.data.wind.speed);
+    windUnitElement.innerHTML = "mi/h";
+  }
 
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 // Default city
 function defaultCity() {
   let cityElement = document.querySelector("#city_choose");
 
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=caucaia&appid=${apiKey}&units=metric`;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=caucaia&appid=${apiKey}&units=${unit}`;
 
   cityElement.innerHTML = "Caucaia";
 
@@ -112,15 +128,28 @@ function defaultCity() {
   axios.get(url).then(currentWeather);
 }
 
+function searchCityByName(cityName) {
+  let cityElement = document.querySelector("#city_choose");
+  let city = cityName.trim()[0].toUpperCase() + cityName.trim().substring(1);
+
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+
+  cityElement.innerHTML = city;
+
+  axios.get(url).then(changeWeatherInformations);
+}
+
 // Update city name
 function changeCity(event) {
   event.preventDefault();
 
   let newCity = document.querySelector("#new_city").value;
+  searchedCity = document.querySelector("#new_city").value;
+
   let cityElement = document.querySelector("#city_choose");
   let city = newCity.trim()[0].toUpperCase() + newCity.trim().substring(1);
 
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
 
   cityElement.innerHTML = city;
 
@@ -130,27 +159,17 @@ function changeCity(event) {
 let inputCity = document.querySelector("#input_city");
 inputCity.addEventListener("submit", changeCity);
 
-function convertToFahrenheit(celsiusTemperature) {
-  return (celsiusTemperature * 9) / 5 + 32;
-}
-
 // Display Units of Temperature
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
+
   celsiusLink.classList.remove("active");
   fahrenheitLink.classList.add("active");
-  let temperatureElement = document.querySelector("#temperature_number");
-  let currentCityElement = document.querySelector("#current_city");
 
-  let fahrenheitTemperature = convertToFahrenheit(celsiusTemperature);
-  let fahrenheitCurrentTemperature = convertToFahrenheit(
-    celsiusCurrentTemperature,
-  );
+  unit = "imperial";
 
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-  currentCityElement.innerHTML = `${currentCity} ${Math.round(
-    fahrenheitCurrentTemperature,
-  )} °F`;
+  defaultCity();
+  if (searchedCity !== null) searchCityByName(searchedCity);
 }
 
 function displayCelsiusTemperature(event) {
@@ -159,18 +178,15 @@ function displayCelsiusTemperature(event) {
   fahrenheitLink.classList.remove("active");
   celsiusLink.classList.add("active");
 
-  let temperatureElement = document.querySelector("#temperature_number");
-  let currentCityElement = document.querySelector("#current_city");
-
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-  currentCityElement.innerHTML = `${currentCity} ${Math.round(
-    celsiusCurrentTemperature,
-  )} °C`;
+  unit = "metric";
+  defaultCity();
+  if (searchedCity !== null) searchCityByName(searchedCity);
 }
 
-let celsiusTemperature = null;
-let celsiusCurrentTemperature = null;
 let currentCity = null;
+let searchedCity = null;
+let unit = "metric";
+let windUnit = "km/h";
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
@@ -179,3 +195,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 defaultCity();
+displayForecast();
